2b06125da26c0dd69ba9bd890bb1df615d9c44e9
Matt Montgomery <mmontgomery@deseretdigital.com>
2017-12-18 15:35:02 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,5 +3,8 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ln -f -s "$DIR"/bash/.bash_git ~/.bash_git
-bash ~/.bash_git
-bash ~/.bash_aliases
+ln -f -s "$DIR"/bash/.bash_aliases ~/.bash_aliases
+ln -f -s "$DIR"/bash/.bash_ls ~/.bash_ls
+source ~/.bash_git
+source ~/.bash_aliases
+source ~/.bash_ls

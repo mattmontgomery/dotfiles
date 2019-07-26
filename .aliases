1913bc4d0d69c6ebd46aedc866063fa27b3fb1f1
Matt Montgomery <montgomery.matt@gmail.com>
2019-07-26 13:55:56 -0600

    Refactor

--- .aliases
+++
@@ -1,1 +0,0 @@
-alias delete_local_branches='git branch --merged | xargs -n 1 git branch -d'
--- setup.sh
+++ setup.sh
@@ -2,9 +2,9 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-ln -f -s "$DIR"/bash/.bash_git ~/.bash_git
-ln -f -s "$DIR"/bash/.bash_aliases ~/.bash_aliases
-ln -f -s "$DIR"/bash/.bash_ls ~/.bash_ls
+ln -f -s "$DIR"/shell/git ~/.bash_git
+ln -f -s "$DIR"/shell/aliases ~/.bash_aliases
+ln -f -s "$DIR"/shell/ls ~/.bash_ls
 source ~/.bash_git
 source ~/.bash_aliases
 source ~/.bash_ls
